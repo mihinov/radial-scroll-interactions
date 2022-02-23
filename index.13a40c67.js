6518a093deb6bdbc93536d75857a30933a4c3499
mihinov <kamahinmihail@gmail.com>
2022-02-23 20:26:45 +0000

    Deploying to gh-pages from @ mihinov/radial-scroll-interactions@25cc8ced6e2e19a2b299e01d4d70c66844124870 🚀

---
+++ index.13a40c67.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){document.querySelector(".content")}));
+//# sourceMappingURL=index.13a40c67.js.map
--- index.e7ef730f.js
+++
@@ -1,2 +0,0 @@
-console.log(3);
-//# sourceMappingURL=index.e7ef730f.js.map
